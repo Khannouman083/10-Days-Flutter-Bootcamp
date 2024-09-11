8402f24fbb529462a7434f6504f48d6fd0cd5f0e
Nouman Khan <khannouman083@gmail.com>
2024-09-11 10:40:03 +0500

    new files uploaded

--- firebase_tutorials/lib/main.dart
+++ firebase_tutorials/lib/main.dart
@@ -1,11 +1,14 @@
 import "package:firebase_core/firebase_core.dart";
+import "package:firebase_tutorials/firebase_options.dart";
 import "package:firebase_tutorials/signup_screen.dart";
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
--- firebase_tutorials/lib/postScreen.dart
+++ firebase_tutorials/lib/postScreen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_tutorials/addpost.dart';
 import 'package:firebase_tutorials/loginScreen.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +13,9 @@ class postScreen extends StatefulWidget {
 }
 
 class _postScreenState extends State<postScreen> {
+  final searchController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+  DatabaseReference dref = FirebaseDatabase.instance.ref("Post");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,13 +32,79 @@ class _postScreenState extends State<postScreen> {
           SizedBox(width: 20,)
         ],
       ),
-      body: Center(
-        child: Text("Post Screen"),
+      body:
+          // StreamBuilder(
+          //     stream: dref.onValue,
+          //     builder: (context,AsyncSnapshot<DatabaseEvent> snapshot){
+          //       if(snapshot.hasData){
+          //         Map<dynamic, dynamic> map = snapshot.data!.snapshot.value as dynamic;
+          //         List<dynamic> list = [];
+          //         list.clear();
+          //         list = map.values.toList();
+          //         return ListView.builder(
+          //             itemCount: snapshot.data!.snapshot.children.length,
+          //             itemBuilder: (context, index){
+          //               return ListTile(
+          //                 title: Text(list[index]['title']) ,
+          //                 subtitle: Text(list[index]['id']) ,
+          //               );
+          //             });
+          //
+          //       }
+          //       else {
+          //         return CircularProgressIndicator();
+          //       }
+          //     }),
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: "Search",
+                suffixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value){
+                setState(() {
+                  
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: dref,
+                itemBuilder: (context, snapshot, animation, index){
+                  String title = snapshot.child("title").value.toString();
+                  if(searchController.text.isEmpty){
+                    return ListTile(
+                      title: Text(snapshot.child("title").value.toString()),
+                      subtitle: Text(snapshot.child("id").value.toString()),
+                    );
+                  }
+                  else if(title.toLowerCase().contains(searchController.text.toLowerCase())){
+                    return ListTile(
+                      title: Text(snapshot.child("title").value.toString()),
+                      subtitle: Text(snapshot.child("id").value.toString()),
+                    );
+                  }
+                  else {
+                    return Container();
+                  }
+                 
+
+                 
+                }),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
         onPressed: (){
-Navigator.push(context, MaterialPageRoute(builder: (context)=>addPost()));
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>addPost()));
+
       },
         child: Icon(Icons.add),),
     );
