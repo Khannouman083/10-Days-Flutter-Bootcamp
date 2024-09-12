d4f187c967063b3b683e23e55ba9a4264fd476cf
Nouman Khan <khannouman083@gmail.com>
2024-09-12 21:16:31 +0500

    new projects uploaded

---
+++ calculator/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:calculator/calculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      home: calculator(),
+    );
+  }
+}
